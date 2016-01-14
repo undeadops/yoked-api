2d35a48431e9107ca78c99f1db010990ae5fd273
Mitch Anderson <mitch@metauser.net>
2016-01-14 13:56:07 -0700

    Changed OS environment for whats being passed in

--- api/config.py
+++ api/config.py
@@ -6,9 +6,9 @@ import os
 class BaseConfig(object):
     SECRET_KEY = os.environ['SECRET_KEY']
     DEBUG = os.environ['DEBUG']
-    DB_NAME = os.environ['POSTGRES_USER']
+    DB_NAME = os.environ['POSTGRES_DB']
     DB_USER = os.environ['POSTGRES_USER']
-    DB_PASS = os.environ['POSTGRES_PASS']
+    DB_PASS = os.environ['POSTGRES_PASSWORD']
     DB_HOST = os.environ['DB_HOST']
     DB_PORT = 5432
     SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
